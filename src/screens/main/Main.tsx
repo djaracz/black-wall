a6d7d4d9d6aefe6043c76276580aff1c8266b91f
djaracz <dawid.jaracz@gmail.com>
2018-06-11 00:45:20 +0200

    resolve conflicts

---
+++ src/screens/main/Main.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NativeRouter, Route, Link } from 'react-router-native'
+import { styles } from './Main.s';
+
+namespace Main {
+  export type DispatchProps = {};
+  export type StateProps = {};
+  export type OwnProps = {};
+  export type Props = StateProps & DispatchProps & OwnProps;
+}
+
+const TempGenericView: React.SFC<{ children?: React.ReactElement<any>; text: string }> = (props) => (
+  <View>
+    <Text>{props.text}</Text>
+  </View>
+);
+
+export const Main: React.SFC<Main.Props> = () => (
+  <NativeRouter>
+    <View style={styles.view}>
+      <Link to="/" underlayColor="pink"><Text>view 1</Text></Link>
+      <Link to="/view2" underlayColor="pink"><Text>view 2</Text></Link>
+      <Link to="/view3" underlayColor="pink"><Text>view 3</Text></Link>
+      <Route path="/" render={() => <TempGenericView text="default" />} exact />
+      <Route path="/view2" render={() => <TempGenericView text="view2" />} exact />
+      <Route path="/view3" render={() => <TempGenericView text="view3" />} exact />
+    </View>
+  </NativeRouter>
+);
