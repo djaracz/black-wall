d379622f72ccea2569844de2a56704ab3c190de5
djaracz <dawid.jaracz@gmail.com>
2018-06-23 11:35:27 +0200

    sync actions complete

--- src/action/CounterAction.ts
+++ src/action/CounterAction.ts
@@ -1,21 +1,23 @@
+import { Action, BaseAction } from 'redux-actions';
 
 export namespace CounterAction {
   export const INCREMENT = 'INCREMENT';
-  export const increment = () => ({ type: INCREMENT });
-
-  export const INCREMENT_ASYNC = 'INCREMENT_ASYNC';
-  export const incrementAsync = () => ({ type: INCREMENT_ASYNC });
+  export type IncrementAction = BaseAction;
+  export const increment = (): IncrementAction => ({ type: INCREMENT });
 
   export const DECREMENT = 'DECREMENT';
-  export const decrement = () => ({ type: DECREMENT });
+  export type DecrementAction = BaseAction;
+  export const decrement = (): DecrementAction => ({ type: DECREMENT });
 
   export const RESET = 'RESET';
-  export const reset = () => ({ type: RESET });
+  export type ResetAction = BaseAction;
+  export const reset = (): ResetAction => ({ type: RESET });
 
   export const SET_VALUE = 'SET_VALUE';
-  export const setValue = (value: number) => ({
+  export type SetValueAction = Action<number>;
+  export const setValue = (payload: number): SetValueAction => ({
     type: SET_VALUE,
-    value
+    payload
   });
 
 }
--- src/reducer/CounterReducer.ts
+++ src/reducer/CounterReducer.ts
@@ -1,3 +1,6 @@
+import reduceReducer from 'reduce-reducers';
+import { combineReducers } from 'redux';
+import { handleAction, Action } from 'redux-actions';
 import { CounterAction } from '../action/CounterAction';
 
 export namespace CounterReducer {
@@ -9,18 +12,52 @@ export namespace CounterReducer {
     value: 0
   };
 
-  export const reducer = (state: State = initial, action: any) => {
-    switch (action.type) {
-      case CounterAction.INCREMENT:
-        return ({ value: state.value + 1 });
-      case CounterAction.DECREMENT:
-        return ({ value: state.value - 1 });
-      case CounterAction.SET_VALUE:
-        return action.payload;
-      case CounterAction.RESET:
-        return initial;
-      default:
-        return state;
-    }
-  }
+  export const increment = handleAction(
+    CounterAction.INCREMENT,
+    (state: State) => ({ value: state.value + 1 }),
+    initial
+  );
+
+  export const decrement = handleAction(
+    CounterAction.DECREMENT,
+    (state: State) => ({ value: state.value - 1 }),
+    initial
+  );
+
+  export const reset = handleAction(
+    CounterAction.RESET,
+    (state: State) => (initial),
+    initial
+  );
+
+  export const setValue = handleAction(
+    CounterAction.SET_VALUE,
+    (state: State, action: CounterAction.SetValueAction) => ({ value: action.payload || state.value }),
+    initial
+  );
+
+  export const reducer = reduceReducer(
+    increment,
+    decrement,
+    reset
+  );
+
+  // export const reducer = handleActions({
+  //   value: combineActions(increment)
+  // }, initial)
+
+  // export const reducer = (state: State = initial, action: any) => {
+  //   switch (action.type) {
+  //     case CounterAction.INCREMENT:
+  //       return ({ value: state.value + 1 });
+  //     case CounterAction.DECREMENT:
+  //       return ({ value: state.value - 1 });
+  //     case CounterAction.SET_VALUE:
+  //       return action.payload;
+  //     case CounterAction.RESET:
+  //       return initial;
+  //     default:
+  //       return state;
+  //   }
+  // }
 }
