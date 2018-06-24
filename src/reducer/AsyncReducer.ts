93e3bd60d848d2709c3015f63e6daf7a5c12c1ef
djaracz <dawid.jaracz@gmail.com>
2018-06-25 01:39:10 +0200

    async data model  error asyn state handler

--- src/reducer/AsyncReducer.ts
+++ src/reducer/AsyncReducer.ts
@@ -1,36 +1,33 @@
 import { AnyAction } from 'redux';
 import { Map } from 'immutable';
 import reduceReducer from 'reduce-reducers';
-import { Async } from '../utils/Async';
-import { ErrorReason } from '../model/ErrorReason';
-import { TypeSelector } from '../selectors/TypeSelector';
 import { AsyncSelector } from '../selectors/AsyncSelector';
+import { AsyncData } from '../model/AsyncData';
+import { ErrorReason } from '../model/ErrorReason';
 
 export namespace AsyncReducer {
-  export type Async = {
-    status: Async.Status,
-    error?: ErrorReason.Type
-  }
-  export type State = Readonly<Map<string, Async>>;
+  export type State = Readonly<Map<string, AsyncData.Type>>;
 
   export const initial: State = Map();
 
   function async(state: State, action: AnyAction) {
     if (
       AsyncSelector.isActionAsync(action)
-      && AsyncSelector.selectTypeExists(action.type)(state)
-      && TypeSelector.isRejected(action.type)
+      && AsyncSelector.isRejected(action)
     ) {
       return state.set(
         action.meta.type,
-        { status: AsyncSelector.selectStatus(action.meta.status), error: action.payload }
+        AsyncData.create(
+          AsyncSelector.selectStatus(action.meta.status),
+          ErrorReason.create(action.payload)
+        )
       );
     } else if (
       AsyncSelector.isActionAsync(action)
     ) {
       return state.set(
         action.meta.type,
-        { status: AsyncSelector.selectStatus(action.meta.status) }
+        AsyncData.create(AsyncSelector.selectStatus(action.meta.status))
       );
     } else {
       return initial;
--- src/selectors/AsyncSelector.ts
+++ src/selectors/AsyncSelector.ts
@@ -3,13 +3,14 @@ import { createSelector } from 'reselect';
 import { Async } from '../utils/Async';
 import { RootState } from '../reducer';
 import { AsyncReducer } from '../reducer/AsyncReducer';
+import { AsyncData } from '../model/AsyncData';
 
 export namespace AsyncSelector {
   export const selectDomain = (state: RootState): AsyncReducer.State => state.async;
 
   export const select = (domain: AsyncReducer.State): AsyncReducer.State => domain;
 
-  export type SelectType = AsyncReducer.Async | undefined;
+  export type SelectType = AsyncData.Type | undefined;
   export const selectType = (type: string) => createSelector(
     select,
     (async: AsyncReducer.State): SelectType => async.get(type)
@@ -31,4 +32,6 @@ export namespace AsyncSelector {
   };
 
   export const isActionAsync = (action: AnyAction): boolean => action.meta && action.meta.async;
+
+  export const isRejected = (action: AnyAction): boolean => action.meta && action.meta.status === Async.Status.REJECTED;
 }
