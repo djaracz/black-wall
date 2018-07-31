dc93ccbc9a9acda3230b1de88ca708c972ee880b
djaracz <dawid.jaracz@gmail.com>
2018-07-31 23:20:16 +0200

    cleanign mapper

--- src/ExampleModule/mapper/PostMapper.ts
+++ src/ExampleModule/mapper/PostMapper.ts
@@ -2,6 +2,5 @@ import { Post } from '../model';
 import { PostService } from '../service';
 
 export namespace PostMapper {
-  export const list = (array: PostService.Entry[]) =>
-    array.map((entry: PostService.Entry) => Post.create(entry));
+  export const list = (array: PostService.Entry[]) => array.map(Post.create);
 }
